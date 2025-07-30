960d2b44ae05bce2521b621323489afff2bc8ef0
ATTAQA <nehahinduja29@gmail.com>
2025-07-30 20:01:55 +0100

    Initial commit

---
+++ src/main/java/Testcases/TC_VideoGameAPI.java
@@ -0,0 +1,11 @@
+package Testcases;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TC_VideoGameAPI {
+    public static void main(String[] args) {
+        SpringApplication.run(TC_VideoGameAPI.class, args);
+    }
+}
